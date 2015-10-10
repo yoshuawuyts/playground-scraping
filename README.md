88e92bd6e03aa0c605d7e17d9960dc6496c2bf4a
Yoshua Wuyts <i@yoshuawuyts.com>
2015-10-10 21:54:46 +1100

    .

---
+++ README.md
@@ -0,0 +1,16 @@
+# playground-scraping
+[![js-standard-style][standard-image]][standard-url]
+
+Little web scraping experiment. Inspired by
+[@louiscntr](https://twitter.com/louiscntr)'s work.
+
+## Installation
+```sh
+$ git clone https://github.com/yoshuawuyts/playground-scraping
+```
+
+## License
+[MIT](https://tldrlegal.com/license/mit-license)
+
+[standard-image]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[standard-url]: https://github.com/feross/standard
---
+++ index.js
@@ -0,0 +1,45 @@
+const stream = require('readable-stream')
+const jsdom = require('jsdom')
+const nets = require('nets')
+const pump = require('pump')
+const fs = require('fs')
+
+const vineUrl = 'https://vine.co/v/e2QalQxOA1K'
+const ws = fs.createWriteStream('backpain.mp4')
+pump(vineScrape(vineUrl), ws, function (err) {
+  if (err) throw err
+  process.stdout.write('succes!')
+})
+
+// scrape a vine video from a url
+// (str, fn) -> rs
+function vineScrape (str, cb) {
+  const ps = new stream.PassThrough()
+
+  jsdom.env(vineUrl, function (err, window) {
+    if (err) return destroy(ps, err)
+    const document = window.document
+
+    const vid = document.querySelector('video')
+    if (!vid) return destroy(ps, 'video element not found')
+
+    const src = vid.getAttribute('src')
+    if (!src) return destroy(ps, 'src attribute not found')
+
+    // wrangle the url into the right format
+    const tail = src.indexOf('?versionId')
+    const downloadUrl = src.slice(0, tail)
+    pump(nets(downloadUrl), ps)
+  })
+
+  return ps
+}
+
+// destroy a stream
+// (stream, str|obj?) -> null
+function destroy (stream, err) {
+  process.nextTick(function () {
+    if (err) stream.emit('error', err)
+    stream.emit('close')
+  })
+}
